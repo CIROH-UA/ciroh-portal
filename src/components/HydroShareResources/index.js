6c74883c9ee69d916efeaacbde705a615c34d6ee
romer8 <gio.rombus@gmail.com>
2025-01-24 17:37:35 -0700

    better style

--- src/components/HydroShareResources/index.js
+++ src/components/HydroShareResources/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import HydroShareResourceCard from './HydroShareResourceCard';
-import styles from './styles.module.css';
+import React from "react";
+import HydroShareResourceCard from "./HydroShareResourceCard";
+import styles from "./styles.module.css";
+import clsx from "clsx"; // Use clsx for combining classes
 
 export default function HydroShareResources({ resources = [] }) {
   return (
+    <div className={clsx("container")}>
       <div className={styles.gridContainer}>
-              {resources.map((res, idx) => (
-                <HydroShareResourceCard resource={res} key={idx} />
-              ))}
+        {resources.map((res, idx) => (
+          <HydroShareResourceCard resource={res} key={idx} />
+        ))}
       </div>
+    </div>
   );
 }
