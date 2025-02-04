f843f958213c642e31d409018a019b0a9ce58cf7
romer8 <gio.rombus@gmail.com>
2025-02-04 14:53:32 -0700

    added more style

--- src/components/Contribute/utils.js
+++ src/components/Contribute/utils.js
@@ -9,22 +9,22 @@ export const contributeAppCards = [
   {
     imgSrcLight: CreateAppImageLight,
     imgSrcDark: CreateAppImageDark,
-    imgAlt: 'Create an App Connector Resource',
-    cardTitle: 'Create an App Connector Resource',
+    imgAlt: 'Create an App Resource',
+    cardTitle: '1. Create an App Resource',
     cardDescription: 'Provide the necessary metadata and URLs to make your app discoverable. Provide an image to make your app visually appealing.',
   },
   {
     imgSrcLight: KeywordsAppImageLight,
     imgSrcDark: KeywordsAppImageDark,
-    imgAlt: 'Add Keywords to your App Connector Resource',
-    cardTitle: 'Add Keywords to your App Connector Resource',
+    imgAlt: 'Add Keywords to your Resource',
+    cardTitle: '2. Add Keywords',
     cardDescription: 'Add nwm_portal_app keyword to your App Connector Resource to make it discoverable.',
   },
   {
     imgSrcLight: AppListImageLight,
     imgSrcDark: AppListImageDark,
     imgAlt: 'View your App in the App List',
-    cardTitle: 'Make your App Public',
+    cardTitle: '3. Make your App Public',
     cardDescription: 'Make your App Connector Resource public to share it with the CIROH community.',
   }
 ];
