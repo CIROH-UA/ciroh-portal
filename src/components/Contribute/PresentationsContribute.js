c74edfb10e3dee716332f0efaf9aa94a25c29f28
James Dolinar <jdolinar@aquaveo.com>
2025-08-26 11:14:33 -0600

    Use production URL for HydroShare OAuth redirect
    - Switches HydroShare OAuth redirect from localhost to production CIROH Portal URL
    - Fixes authentication flow to redirect users to the correct location after OAuth
    - Removes unnecessary leftover comment

--- src/components/Contribute/PresentationsContribute.js
+++ src/components/Contribute/PresentationsContribute.js
@@ -13,8 +13,6 @@ export default function PresentationsTabContributeContent() {
   return (
           
     <div className={styles.currentAppSection}>
-      {/* <CardsHeader header="Add your Presentation!" />
-       */}
         <div className={Appstyles.headerContainer}>
             <h3 className={Appstyles.description2}>
                 <div>
