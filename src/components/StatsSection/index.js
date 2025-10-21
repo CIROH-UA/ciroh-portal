5634d382b5056550396dd342cd14d5bc0985ef8d
romer8 <gio.rombus@gmail.com>
2025-10-21 13:37:54 -0600

    Update hero header

--- src/components/StatsSection/index.js
+++ src/components/StatsSection/index.js
@@ -199,7 +199,7 @@ export default function StatsSection() {
 
   return (
     <HeroSection
-      description="At CIROH, our team of researchers, hydrologists, 
+      description="Our team of researchers, hydrologists, 
         and engineers is committed to advancing our understanding of hydrologic processes, 
         improving operational hydrologic forecasting techniques and workflows."
     >
