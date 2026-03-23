721efa15fc27351317ef7d37c9cf730b9ef9cffe
Sheargrub <sheargrub@gmail.com>
2026-03-23 12:21:57 -0500

    Adjust Develop redir

--- src/pages/develop/index.js
+++ src/pages/develop/index.js
@@ -1,13 +1,14 @@
 import RedirectHero from '@site/src/components/RedirectHero';
 import Layout from '@theme/Layout';
 
-// !!! THIS PAGE NEEDS AN EQUIVALENT ON HUB !!!
-// For now, the apps page is the closest target, but it's not an appropriate one.
+// 03/23/2026: As of today, this page doesn't have an equivalent on the production version of Hub.
+// I've submitted a PR to address this: https://github.com/CIROH-UA/ciroh_hub/pull/80
+// As such, the link this redirects to is currently dead, but it'll be live and working by the time these redirects are published.
 
 export default function RedirPage() {
   return (
     <Layout title="CIROH Hub Redirect" description="Redirect from CIROH Portal to CIROH Hub.">
-      <RedirectHero href="https://hub.ciroh.org/apps/" />
+      <RedirectHero href="https://hub.ciroh.org/contribute/develop/" />
     </Layout>
   );
 }
