2ea12fd2112b805e07b05178c56ee2306f8ca270
Sheargrub <sheargrub@gmail.com>
2026-03-24 11:33:13 -0500

    Fix build errors

--- src/components/RedirectHero/RedirectHandler.js
+++ src/components/RedirectHero/RedirectHandler.js
@@ -3,12 +3,12 @@ import BrowserOnly from '@docusaurus/BrowserOnly';
 // delayTime provided in seconds
 export default function RedirectHandler({ href, delayTime }) {
   return (
-      <BrowserOnly>
-        <span>
-          <script>
-            {setTimeout(() => location.href = href, delayTime * 1000)}
-          </script>
-        </span>
-      </BrowserOnly>
+    <BrowserOnly>
+      {() => {
+        <script>
+          {setTimeout(() => location.href = href, delayTime * 1000)}
+        </script>
+      }}
+    </BrowserOnly>
   );
 }
